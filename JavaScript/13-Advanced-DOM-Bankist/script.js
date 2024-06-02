bf0cd949518df20bc68cdaebfbcd383fe61b0c04
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2024-06-02 11:09:01 +0100

    [LEARNING]-JavaScript: Implement page navigation using Event Delegation.

--- JavaScript/13-Advanced-DOM-Bankist/script.js
+++ JavaScript/13-Advanced-DOM-Bankist/script.js
@@ -1,12 +1,11 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,13 +29,32 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-// Implement Smooth Scrolling
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
+/////////////////////////////////////////////////
+// Smooth Scrolling
 btnScrollTo.addEventListener('click', function (e) {
   e.preventDefault();
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+/////////////////////////////////////////////////
+// Page Navigation
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     const section = document.querySelector(id);
+//     section.scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    const section = document.querySelector(id);
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
 });
 
 /////////////////////////////////////////////////
@@ -220,5 +238,3 @@ const eventPropagation = function () {
     false // true = catch events on capturing phase
   );
 };
-
-eventPropagation();
