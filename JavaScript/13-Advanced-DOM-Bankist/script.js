73df1a6cee6d94904a9e4c1e69ed60d6af82b6d9
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2024-06-07 22:34:18 +0200

    [LEARNING]-JavaScript: Lazy Loading Images and Reveal Sections on sceoll using Intersection Observer API.

--- JavaScript/13-Advanced-DOM-Bankist/script.js
+++ JavaScript/13-Advanced-DOM-Bankist/script.js
@@ -132,6 +132,53 @@ const headerObserver = new IntersectionObserver(stickyNav, observerOptions);
 headerObserver.observe(header);
 
 /////////////////////////////////////////////////
+// Reveal sections
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+const allSections = document.querySelectorAll('.section');
+allSections.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+/////////////////////////////////////////////////
+// Lazy Loading Images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+/////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 let message;
