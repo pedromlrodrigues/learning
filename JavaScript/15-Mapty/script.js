9381c573a100a880c87545aa9f11750054a34254
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2024-06-30 11:34:34 +0100

    [LEARNING]-JavaScript: Setup Leaflet map and display custom marker.

--- JavaScript/15-Mapty/script.js
+++ JavaScript/15-Mapty/script.js
@@ -11,14 +11,44 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-navigator.geolocation.getCurrentPosition(
-  function (position) {
-    console.log(position);
-    const { latitude, longitude } = position.coords;
-    console.log(latitude, longitude);
-    console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
-  },
-  function () {
-    alert('Could not get your position');
-  }
-);
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude, longitude } = position.coords;
+      var map = L.map('map').setView([latitude, longitude], 13);
+
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([latitude, longitude])
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+
+      map.on('click', function (event) {
+        console.log(event);
+        const { lat, lng } = event.latlng;
+        console.log(lat, lng);
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              minWidth: 100,
+              maxWidth: 250,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+}
