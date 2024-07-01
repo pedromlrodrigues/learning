e5fdead61b839046bbd8771a676981bd42f9145f
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2024-07-01 12:23:24 +0100

    [LEARNING]-JavaScript: Implement Workout, Running and Cycling classes.

--- JavaScript/15-Mapty/script.js
+++ JavaScript/15-Mapty/script.js
@@ -11,6 +11,52 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  id = Date.now().toString().slice(-10);
+  date = new Date();
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat,lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in minutes
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevation) {
+    super(coords, distance, duration);
+    this.elevation = elevation;
+
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+  }
+}
+
+const run1 = new Running([40, -12], 5.2, 24, 178);
+console.log(run1);
+const cycling1 = new Cycling([50, -12], 27, 95, 657);
+console.log(cycling1);
+
+/////////////////////////////////////////////////////////////////////////////
+// APPLICATION ARCHITECTURE
 class App {
   #map;
   #mapEvent;
