d589523d9c476f3ddbc8a6c722414b2488495fbc
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2024-08-26 20:35:41 +0100

    [LEARNING]-SpringBoot: Custom application properties injection.

--- Spring Boot/01-Overview/04-custom-application-properties/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ Spring Boot/01-Overview/04-custom-application-properties/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,17 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${car.brand}")
+    private String brand;
+
+    @Value("${car.model}")
+    private String model;
 
     @GetMapping("/")
     public String sayHello() {
@@ -20,4 +27,7 @@ public class FunRestController {
     public String getDevTools() {
         return "Spring Boot Dev Tools are cool!";
     }
+
+    @GetMapping("/what-car")
+    public String getCar() { return String.format("Brand: %s Model: %s", brand, model); }
 }
