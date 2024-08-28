3c1ae66232c45f8dd25aff1fe861e9255c298ba5
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2024-08-28 09:07:50 +0100

    [LEARNING]-SpringBoot: Dependency Injection using constructor injection (Start of Section 2).

---
+++ Spring Boot/02-Spring-Core/01-dependency-injection/springcoredemo/src/main/java/com/peterlimz/springcoredemo/DemoController.java
@@ -0,0 +1,29 @@
+package com.peterlimz.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private final Coach coach;
+
+    /**
+     * Single Constructor Injection: In Spring Framework 4.3 and later,
+     *  if a class has only one constructor, Spring will automatically use that
+     *  constructor for dependency injection, even if the @Autowired annotation is not present.
+     *  This is called implicit constructor injection.
+     *  Since your DemoController class has only one constructor,
+     *  Spring automatically injects the Coach dependency without requiring the @Autowired annotation.
+     * */
+    @Autowired
+    DemoController(Coach coach) {
+        this.coach = coach;
+    }
+
+    @GetMapping("/get-daily-workout")
+    public String getDailyWorkout() {
+        return coach.getDailyWorkout();
+    }
+}
