866bc652e647e659e726ac0500a2f863d4532428
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2024-09-05 23:26:51 +0100

    [LEARNING]-SpringBoot: Create database tables using Java (End of Section 3).
    - Set Hibernate DDL auto property to create (drops existing tables and creates tables according to the entities defined in Java considering also the annotations for the Primary Key and also the other columns;
    - Set log level to "debug" for Hibernate SQL statements;
    - Set log level to "trace" for Hibernate ORM JDBC bind.

---
+++ Spring Boot/03-Hibernate-Jpa-Crud/02-create-database-tables-from-java-code/src/main/java/com/peterlimz/hibernate_jpa_crud_demo/HibernateJpaCrudDemoApplication.java
@@ -0,0 +1,12 @@
+package com.peterlimz.hibernate_jpa_crud_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateJpaCrudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateJpaCrudDemoApplication.class, args);
+    }
+}
