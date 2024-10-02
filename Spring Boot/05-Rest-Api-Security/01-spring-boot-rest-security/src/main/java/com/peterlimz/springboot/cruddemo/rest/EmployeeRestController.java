c390a69c5feb4c953d288013391cdcfb2a8853c1
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2024-10-02 23:29:55 +0100

    [LEARNING]-SpringBoot: Restrict URLs based on Roles.
    - The InMemoryUserDetailsManager in the @Configuration class overrides the previously configured user and password in the application.properties.

--- Spring Boot/05-Rest-Api-Security/01-spring-boot-rest-security/src/main/java/com/peterlimz/springboot/cruddemo/rest/EmployeeRestController.java
+++ Spring Boot/05-Rest-Api-Security/01-spring-boot-rest-security/src/main/java/com/peterlimz/springboot/cruddemo/rest/EmployeeRestController.java
@@ -3,7 +3,14 @@ package com.peterlimz.springboot.cruddemo.rest;
 import com.peterlimz.springboot.cruddemo.entity.Employee;
 import com.peterlimz.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -25,7 +32,6 @@ public class EmployeeRestController {
     }
 
     // add mapping for GET /employees/{employeeId}
-
     @GetMapping("/employees/{employeeId}")
     public Employee getEmployee(@PathVariable int employeeId) {
 
