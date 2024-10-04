314a85fdddb52a7e5fd3c8f76d65583bdaf22d61
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2024-10-04 23:50:40 +0100

    [LEARNING]-SpringBoot: @OneToMany and @ManyToOne.

---
+++ Spring Boot/09-Jpa-Hibernate-Advanced-Mappings/03-jpa-one-to-many/src/main/java/com/peterlimz/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,54 @@
+package com.peterlimz.cruddemo.dao;
+
+import com.peterlimz.cruddemo.entity.Instructor;
+import com.peterlimz.cruddemo.entity.InstructorDetail;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class AppDAOImpl implements AppDAO {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
+    @Override
+    @Transactional
+    public void save(Instructor instructor) {
+        entityManager.persist(instructor);
+    }
+
+    @Override
+    public Instructor findInstructorById(int id) {
+        return entityManager.find(Instructor.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int id) {
+        Instructor instructor = findInstructorById(id);
+
+        entityManager.remove(instructor);
+    }
+
+    @Override
+    public InstructorDetail findInstructorDetailById(int id) {
+        return entityManager.find(InstructorDetail.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorDetailById(int id) {
+        InstructorDetail instructorDetail = findInstructorDetailById(id);
+
+        instructorDetail.getInstructor().setInstructorDetail(null);
+
+        entityManager.remove(instructorDetail);
+    }
+}
---
+++ Spring Boot/09-Jpa-Hibernate-Advanced-Mappings/03-jpa-one-to-many/src/main/java/com/peterlimz/cruddemo/entity/Course.java
@@ -0,0 +1,41 @@
+package com.peterlimz.cruddemo.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString(exclude = "instructor")
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+
+    public Course(String title) {
+        this.title = title;
+    }
+}
