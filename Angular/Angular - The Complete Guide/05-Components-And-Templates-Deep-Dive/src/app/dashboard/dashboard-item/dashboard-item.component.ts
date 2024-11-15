729cfa6c0d78e4c80b250e4ce3f7fde689826aad
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2024-11-15 00:18:54 +0000

    [LEARNING]-Angular: Split components.

---
+++ Angular/Angular - The Complete Guide/05-Components-And-Templates-Deep-Dive/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-item.component.html',
+  styleUrl: './dashboard-item.component.css',
+})
+export class DashboardItemComponent {
+  @Input({ required: true }) imgPath!: string;
+  @Input({ required: true }) imgAlt!: string;
+  @Input({ required: true }) title!: string;
+  currentStatus = 'online';
+}
---
+++ Angular/Angular - The Complete Guide/05-Components-And-Templates-Deep-Dive/src/app/dashboard/tickets/ticket-item/ticket-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './ticket-item.component.html',
+  styleUrl: './ticket-item.component.css'
+})
+export class TicketItemComponent {
+
+}
