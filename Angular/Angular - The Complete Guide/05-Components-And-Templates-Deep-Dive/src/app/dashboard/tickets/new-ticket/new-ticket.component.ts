11b8c9934da694ce289fdfd68d6ec57ca1bd42d3
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2024-11-15 00:40:21 +0000

    [LEARNING]-Angular: Content projection with Multiple Slots.
    - Configure 'select' property in ng-content in order to match elements with the defined class name.

--- Angular/Angular - The Complete Guide/05-Components-And-Templates-Deep-Dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ Angular/Angular - The Complete Guide/05-Components-And-Templates-Deep-Dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [],
   templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  styleUrl: './new-ticket.component.css',
+  imports: [FormsModule, ButtonComponent],
 })
-export class NewTicketComponent {
-
-}
+export class NewTicketComponent {}
