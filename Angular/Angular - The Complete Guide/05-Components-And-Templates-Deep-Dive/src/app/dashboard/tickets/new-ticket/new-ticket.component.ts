0cc4c0850026a80cb2894e4fc557c3af62fa1ea1
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2024-11-16 20:09:54 +0000

    [LEARNING]-Angular: Scoping CSS Styles to Components.

--- Angular/Angular - The Complete Guide/05-Components-And-Templates-Deep-Dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ Angular/Angular - The Complete Guide/05-Components-And-Templates-Deep-Dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { FormsModule } from '@angular/forms';
+import { ControlComponent } from '../../../shared/control/control.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
-  imports: [FormsModule, ButtonComponent],
+  imports: [FormsModule, ButtonComponent, ControlComponent],
 })
 export class NewTicketComponent {}
