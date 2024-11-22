69d3d50341ba4c62d860b302518bdf48465da2f1
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2024-11-22 00:24:46 +0000

    [LEARNING]-Angular: Using @ViewChild to access Template elements.

--- Angular/Angular - The Complete Guide/05-Components-And-Templates-Deep-Dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ Angular/Angular - The Complete Guide/05-Components-And-Templates-Deep-Dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { FormsModule } from '@angular/forms';
 import { ControlComponent } from '../../../shared/control/control.component';
@@ -11,7 +11,10 @@ import { ControlComponent } from '../../../shared/control/control.component';
   imports: [FormsModule, ButtonComponent, ControlComponent],
 })
 export class NewTicketComponent {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+
   onSubmit(titleElement: string, requestElement: string) {
     console.log(titleElement, requestElement);
+    this.form?.nativeElement.reset();
   }
 }
