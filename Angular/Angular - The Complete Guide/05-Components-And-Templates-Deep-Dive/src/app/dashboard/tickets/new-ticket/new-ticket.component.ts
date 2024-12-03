0f855b0485cf137858cdf893b340a45efb744412
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2024-12-03 00:59:10 +0000

    [LEARNING]-Angular: @AfterViewInit, @AfterContentInit.

--- Angular/Angular - The Complete Guide/05-Components-And-Templates-Deep-Dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ Angular/Angular - The Complete Guide/05-Components-And-Templates-Deep-Dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  viewChild,
+  ViewChild,
+} from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { FormsModule } from '@angular/forms';
 import { ControlComponent } from '../../../shared/control/control.component';
@@ -10,9 +17,18 @@ import { ControlComponent } from '../../../shared/control/control.component';
   styleUrl: './new-ticket.component.css',
   imports: [FormsModule, ButtonComponent, ControlComponent],
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements AfterViewInit, OnInit {
   // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  ngOnInit() {
+    console.log(this.form().nativeElement);
+  }
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT');
+    console.log(this.form().nativeElement);
+  }
 
   onSubmit(titleElement: string, requestElement: string) {
     console.log(titleElement, requestElement);
