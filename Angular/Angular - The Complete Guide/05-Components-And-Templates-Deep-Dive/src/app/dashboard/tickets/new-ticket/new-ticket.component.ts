77780f4c7e4caf8de7847b40050d367297ed2c6b
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2024-12-04 00:58:32 +0000

    [LEARNING]-Angular: Template For Loops (special variables and @empty).

--- Angular/Angular - The Complete Guide/05-Components-And-Templates-Deep-Dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ Angular/Angular - The Complete Guide/05-Components-And-Templates-Deep-Dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -3,8 +3,8 @@ import {
   Component,
   ElementRef,
   OnInit,
+  output,
   viewChild,
-  ViewChild,
 } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { FormsModule } from '@angular/forms';
@@ -20,6 +20,7 @@ import { ControlComponent } from '../../../shared/control/control.component';
 export class NewTicketComponent implements AfterViewInit, OnInit {
   // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  addTicket = output<{ title: string; text: string }>();
 
   ngOnInit() {
     console.log(this.form().nativeElement);
@@ -30,8 +31,8 @@ export class NewTicketComponent implements AfterViewInit, OnInit {
     console.log(this.form().nativeElement);
   }
 
-  onSubmit(titleElement: string, requestElement: string) {
-    console.log(titleElement, requestElement);
+  onSubmit(title: string, ticketText: string) {
+    this.addTicket.emit({ title: title, text: ticketText });
     this.form().nativeElement.reset();
   }
 }
--- Angular/Angular - The Complete Guide/05-Components-And-Templates-Deep-Dive/src/app/dashboard/tickets/tickets.component.ts
+++ Angular/Angular - The Complete Guide/05-Components-And-Templates-Deep-Dive/src/app/dashboard/tickets/tickets.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
+import { Ticket } from './ticket.model';
+import { TicketItemComponent } from './ticket-item/ticket-item.component';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, TicketItemComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
 })
-export class TicketsComponent {}
+export class TicketsComponent {
+  tickets: Ticket[] = [];
+
+  onAddTicket(newTicket: { title: string; text: string }) {
+    const ticket: Ticket = {
+      title: newTicket.title,
+      request: newTicket.text,
+      id: Math.random().toString(),
+      status: 'open',
+    };
+
+    this.tickets.push(ticket);
+  }
+}
