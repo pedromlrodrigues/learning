0e890c463eb7fb3382b4325363d44133e65fc46c
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2024-12-04 01:11:19 +0000

    [LEARNING]-Angular: Updating Signals.
    signal.set() does almost the same as signal.update().
    The difference is that the signal.update expects a function and provides a parameter with the old value.

--- Angular/Angular - The Complete Guide/05-Components-And-Templates-Deep-Dive/src/app/dashboard/tickets/ticket-item/ticket-item.component.ts
+++ Angular/Angular - The Complete Guide/05-Components-And-Templates-Deep-Dive/src/app/dashboard/tickets/ticket-item/ticket-item.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket-item',
   standalone: true,
   imports: [],
   templateUrl: './ticket-item.component.html',
-  styleUrl: './ticket-item.component.css'
+  styleUrl: './ticket-item.component.css',
 })
 export class TicketItemComponent {
+  data = input.required<Ticket>();
+  detailsVisible = signal(false);
 
+  onToggleDetails() {
+    // this.detailsVisible.set(!this.detailsVisible());
+    this.detailsVisible.update(wasVisible => !wasVisible);
+  }
 }
