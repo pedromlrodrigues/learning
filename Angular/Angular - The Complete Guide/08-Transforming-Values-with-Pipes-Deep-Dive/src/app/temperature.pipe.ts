7b2b6bd5c1e2942b9345261b32573fa69a9024c3
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2025-03-01 10:18:25 +0000

    [LEARNING]-Angular: Building a custom temperature pipe.

---
+++ Angular/Angular - The Complete Guide/08-Transforming-Values-with-Pipes-Deep-Dive/src/app/temperature.pipe.ts
@@ -0,0 +1,43 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'temperature',
+  standalone: true,
+})
+export class TemperaturePipe implements PipeTransform {
+  transform(
+    value: string | number | null,
+    inputType: 'cel' | 'fah',
+    outputType?: 'cel' | 'fah'
+  ) {
+    if (!value) {
+      return value;
+    }
+
+    let val: number;
+    if (typeof value === 'string') {
+      val = parseFloat(value);
+    } else {
+      val = value;
+    }
+
+    let outputTemp: number;
+    if (inputType === 'cel' && outputType === 'fah') {
+      outputTemp = val * (9 / 5) + 32;
+    } else if (inputType === 'fah' && outputType === 'cel') {
+      outputTemp = (val - 32) * (5 / 9);
+    } else {
+      outputTemp = val;
+    }
+
+    let symbol: 'ºC' | 'ºF';
+
+    if (!outputType) {
+      symbol = inputType === 'cel' ? 'ºC' : 'ºF';
+    } else {
+      symbol = outputType === 'cel' ? 'ºC' : 'ºF';
+    }
+
+    return `${outputTemp.toFixed(2)} ${symbol}`;
+  }
+}
