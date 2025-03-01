27c4d360227f3287c979f1b99c45805a7bfeac18
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2025-03-01 12:32:35 +0000

    [LEARNING]-Angular: Create a Service and setup Dependency Injection.

--- Angular/Angular - The Complete Guide/09-Understanding-Services-and-Dependency-Injection-Deep-Dive/src/app/tasks/tasks-list/tasks-list.component.ts
+++ Angular/Angular - The Complete Guide/09-Understanding-Services-and-Dependency-Injection-Deep-Dive/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
+import { TasksService } from '../tasks.service';
 import { TaskItemComponent } from './task-item/task-item.component';
 
 @Component({
@@ -10,8 +11,27 @@ import { TaskItemComponent } from './task-item/task-item.component';
   imports: [TaskItemComponent],
 })
 export class TasksListComponent {
-  selectedFilter = signal<string>('all');
-  tasks = [];
+  private selectedFilter = signal<string>('all');
+  private tasksService = inject(TasksService);
+
+  tasks = computed(() => {
+    switch (this.selectedFilter()) {
+      case 'open':
+        return this.tasksService
+          .allTasks()
+          .filter((task) => task.status === 'OPEN');
+      case 'in-progress':
+        return this.tasksService
+          .allTasks()
+          .filter((task) => task.status === 'IN_PROGRESS');
+      case 'done':
+        return this.tasksService
+          .allTasks()
+          .filter((task) => task.status === 'DONE');
+      default:
+        return this.tasksService.allTasks();
+    }
+  });
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
