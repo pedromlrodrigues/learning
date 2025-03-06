b94e3a055750673670b8624000b8129ba5be2ecf
Pedro Rodrigues <pmlrodrigues14@gmail.com>
2025-03-06 19:19:57 +0000

    [LEARNING]-Angular: Add HTTP error handling.

--- Angular/Angular - The Complete Guide/12-Sending-HTTP-Requests-And-Handling-Responses/src/app/places/available-places/available-places.component.ts
+++ Angular/Angular - The Complete Guide/12-Sending-HTTP-Requests-And-Handling-Responses/src/app/places/available-places/available-places.component.ts
@@ -1,7 +1,7 @@
 import { Component, DestroyRef, inject, signal } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 import { Place } from '../place.model';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
@@ -19,15 +19,31 @@ export class AvailablePlacesComponent {
 
   places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
+  error = signal('');
 
   ngOnInit() {
     this.isFetching.set(true);
     const subscription = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
-      .pipe(map((resData) => resData.places))
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.error(error);
+
+          return throwError(
+            () =>
+              new Error(
+                'Something went wrong fetching the available places. Please try again later!'
+              )
+          );
+        })
+      )
       .subscribe({
         next: (places) => {
           this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
         },
         complete: () => this.isFetching.set(false),
       });
